599fddae91fc72d0fd61d81412f5c6fd58e7670f
sb8956 <sb_8956@naver.com>
2022-10-04 15:59:24 +0900

    비밀번호 검증 예제

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import EventPractice from './EventPractice'
+import ValidationSample from './ValidationSample'
+import { Component } from 'react';
 
-function App() {
-  return(
-    <EventPractice/>
-  )
+class App extends Component {
+  render() {
+    return (
+      <ValidationSample />
+    )
+  }
 }
 
 export default App;
