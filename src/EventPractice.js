8022049a9a262ca0ca1cecc85ea86a1b44eeda05
sb8956 <sb_8956@naver.com>
2022-10-04 14:28:37 +0900

    state에 input 값 담기

---
+++ src/EventPractice.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class EventPractice extends Component{
+
+    state = {
+        message : ''
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>이벤트 연습</h1>
+                <input
+                type="text"
+                name="message"
+                placeholder="아무거나 입력하세요"
+                value={this.state.message}
+                onChange={
+                    (e) => {
+                        this.setState({
+                            message: e.target.value
+                        })
+                    }
+                }
+                ></input>
+            </div>
+        )
+    }
+}
+
+export default EventPractice;
