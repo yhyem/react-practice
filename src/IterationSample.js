4f04df6f031fb537cb1f2c8d29204c7a052d0a9c
sb8956 <sb_8956@naver.com>
2022-10-06 09:57:00 +0900

    데이터 추가 기능

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -6,10 +6,25 @@ const IterationSample = () => {
     const [inputText, setInputText] = useState('');
     const [nextId, setNextId] = useState(5);
     const nameList = names.map((name, index) => <li key={name.id}>{name.text}</li>);
+
+    const onChange = (e) => setInputText(e.target.value);
+    const onClick = () => {
+        const nextNames = names.concat({
+            id: nextId,
+            text: inputText
+        })
+        setNames(nextNames);
+        setInputText('');
+        setNextId(nextId + 1);
+    }
     return (
-        <ul>
-            {nameList}
-        </ul>
+        <>
+            <ul>
+                {nameList}
+            </ul>
+            <input value={inputText} onChange={onChange} />
+            <button onClick={onClick}>추가</button>
+        </>
     )
 }
 
