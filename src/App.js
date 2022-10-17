00b6437bf2f05ffcb9e9ecdd6120ddd32d811493
sb8956 <sb_8956@naver.com>
2022-10-17 15:23:00 +0900

    라이프 사이클 메서드 예제

--- src/App.js
+++ src/App.js
@@ -4,11 +4,29 @@ import EventPractice from './EventPractice'
 import ValidationSample from './ValidationSample'
 import { Component } from 'react';
 import IterationSample from './IterationSample';
+import LifeCycleSample from './LifeCycleSample';
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: '#000000'
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  }
+
   render() {
     return (
-      <IterationSample />
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color}>s</LifeCycleSample>
+      </div>
     )
   }
 }
