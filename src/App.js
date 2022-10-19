7f14dda112203859483aa1cc553b5a90bd0dca85
sb8956 <sb_8956@naver.com>
2022-10-19 09:59:05 +0900

    hooks - useState 사용

--- src/App.js
+++ src/App.js
@@ -6,32 +6,36 @@ import { Component } from 'react';
 import IterationSample from './IterationSample';
 import LifeCycleSample from './LifeCycleSample';
 import ErrorBoundary from './ErrorBoundary';
+import Counter from './Hooks/Counter';
 
-function getRandomColor() {
-  return '#' + Math.floor(Math.random() * 16777215).toString(16);
-}
+// function getRandomColor() {
+//   return '#' + Math.floor(Math.random() * 16777215).toString(16);
+// }
 
-class App extends Component {
-  state = {
-    color: '#000000'
-  }
+// class App extends Component {
+//   state = {
+//     color: '#000000'
+//   }
 
-  handleClick = () => {
-    this.setState({
-      color: getRandomColor()
-    });
-  }
+//   handleClick = () => {
+//     this.setState({
+//       color: getRandomColor()
+//     });
+//   }
 
-  render() {
-    return (
-      <div>
-        <button onClick={this.handleClick}>랜덤 색상</button>
-        <ErrorBoundary>
-          <LifeCycleSample color={this.state.color} />
-        </ErrorBoundary>
-      </div>
-    )
-  }
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.handleClick}>랜덤 색상</button>
+//         <ErrorBoundary>
+//           <LifeCycleSample color={this.state.color} />
+//         </ErrorBoundary>
+//       </div>
+//     )
+//   }
+// }
+const App = () => {
+  return <Counter />
 }
 
 export default App;
