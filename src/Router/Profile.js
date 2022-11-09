cee62e18d28f40834e6fe677dc25ada899802ebb
sb8956 <sb_8956@naver.com>
2022-11-09 17:27:36 +0900

    리액트 라우터 부가기능 : withRouter

--- src/Router/Profile.js
+++ src/Router/Profile.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
+import WithRouterSample from './WithRouterSample';
 
 const data = {
     velopert: {
@@ -21,6 +23,7 @@ const Profile = ({ match }) => {
         <div>
             <h3>{username}({profile.name})</h3>
             <p>{profile.description}</p>
+            <WithRouterSample />
         </div>
     )
 }
