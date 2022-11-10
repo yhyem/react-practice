4a9e19f5caa85818982491c1e140cb194ec67a0e
sb8956 <sb_8956@naver.com>
2022-11-10 17:57:33 +0900

    axios.get - async/await 이용

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import axios from "axios";
 
 const App = () => {
   const [data, setData] = useState(null);
-  const onClick = () => {
-    axios.get('https://jsonplaceholder.typicode.com/todos/1').then(response => {
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/todos/1',
+      );
       setData(response.data);
-    })
+    }
+    catch (e) {
+      console.log(e);
+    }
   }
   return (
     <div>
       <div>
         <button onClick={onClick}>불러오기</button>
       </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />};
+      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
     </div>
   )
 }
