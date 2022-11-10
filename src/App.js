9b4c83bd1cf0abb31c934eea7518a1551747c088
sb8956 <sb_8956@naver.com>
2022-11-10 18:53:49 +0900

    뉴스 뷰어 UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React, { useState } from "react";
 import axios from "axios";
+import NewsList from "./components/NewsList";
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=%REACT_APP_API_KEY%'
-      );
-      setData(response.data);
-    }
-    catch (e) {
-      console.log(e);
-    }
-  }
+  // const [data, setData] = useState(null);
+  // const onClick = async () => {
+  //   try {
+  //     const response = await axios.get(
+  //       'https://newsapi.org/v2/top-headlines?country=kr&apiKey=%REACT_APP_API_KEY%'
+  //     );
+  //     setData(response.data);
+  //   }
+  //   catch (e) {
+  //     console.log(e);
+  //   }
+  // }
   return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
-    </div>
+    <NewsList />
   )
 }
 
---
+++ src/components/NewsList.js
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+import NewsItem from "./NewsItem";
+
+const NewsListBlock = styled.div`
+    box-sizing: border-box;
+    padding-bottom: 3rem;
+    width: 768px;
+    margin: 0 auto;
+    margin-top: 2rem;
+    @media screen and (max-width: 768px){
+        width: 100%;
+        padding-left: 1rem;
+        padding-right: 1rem;
+    }
+`;
+
+const sampleArticle = {
+    title: '제목',
+    description: '내용',
+    url: 'http://google.com',
+    urlToImage: 'https://via.placeholder.com/160'
+};
+
+const NewsList = () => {
+    return (
+        <NewsListBlock>
+            <NewsItem article={sampleArticle}></NewsItem>
+            <NewsItem article={sampleArticle}></NewsItem>
+            <NewsItem article={sampleArticle}></NewsItem>
+            <NewsItem article={sampleArticle}></NewsItem>
+            <NewsItem article={sampleArticle}></NewsItem>
+            <NewsItem article={sampleArticle}></NewsItem>
+            <NewsItem article={sampleArticle}></NewsItem>
+        </NewsListBlock>
+    )
+}
+
+export default NewsList;
