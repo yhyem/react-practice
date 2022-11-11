4c20288dddc6fbfefbe4b39cfd92539bbc5665c6
sb8956 <sb_8956@naver.com>
2022-11-11 22:53:08 +0900

    api 데이터 연동하기

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import NewsItem from "./NewsItem";
+import axios from 'axios';
 
 const NewsListBlock = styled.div`
     box-sizing: border-box;
@@ -23,15 +24,37 @@ const sampleArticle = {
 };
 
 const NewsList = () => {
+    const [articles, setArticles] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoadings(true);
+            try {
+                const response = await axios.get(
+                    'https://newsapi.org/v2/top-headlines?country=kr&apiKey=%REACT_APP_API_KEY%',
+                );
+                setArticles(response.data.articles);
+            } catch (e) {
+                console.log(e);
+            }
+            setLoading(false);
+        };
+        fetchData();
+    }, []);
+
+    if (loading) {
+        return <NewsListBlock>대기 중 ...</NewsListBlock>
+    }
+
+    if (!articles) {
+        return null;
+    }
     return (
         <NewsListBlock>
-            <NewsItem article={sampleArticle}></NewsItem>
-            <NewsItem article={sampleArticle}></NewsItem>
-            <NewsItem article={sampleArticle}></NewsItem>
-            <NewsItem article={sampleArticle}></NewsItem>
-            <NewsItem article={sampleArticle}></NewsItem>
-            <NewsItem article={sampleArticle}></NewsItem>
-            <NewsItem article={sampleArticle}></NewsItem>
+            {articles.map(article => (
+                <NewsItem key={article.url} article={article} />
+            ))}
         </NewsListBlock>
     )
 }
