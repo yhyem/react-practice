bf879eab27ae12625024da3c5f186f0337a69a25
sb8956 <sb_8956@naver.com>
2022-11-14 11:17:46 +0900

    API 호출 시 카테고리 지정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import axios from "axios";
 import NewsList from "./components/NewsList";
 import Categories from "./components/Categories";
 
 const App = () => {
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback(category => setCategory(category), []);
   // const [data, setData] = useState(null);
   // const onClick = async () => {
   //   try {
@@ -18,8 +20,8 @@ const App = () => {
   // }
   return (
     <>
-      <Categories />
-      <NewsList />
+      <Categories category={category} onSelect={onSelect} />
+      <NewsList category={category} />
     </>
   )
 }
--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -24,17 +24,17 @@ const sampleArticle = {
     urlToImage: 'https://via.placeholder.com/160'
 };
 
-const NewsList = () => {
+const NewsList = ({ category }) => {
     const [articles, setArticles] = useState(null);
     const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         const fetchData = async () => {
             setLoading(true);
-            console.log(process.env.REACT_APP_API_KEY)
             try {
+                const query = category === 'all' ? '' : `&category=${category}`;
                 const response = await axios.get(
-                    'https://newsapi.org/v2/top-headlines?country=kr&apiKey=%REACT_APP_API_KEY%'
+                    `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=${apiKey}`
                 );
                 setArticles(response.data.articles);
             } catch (e) {
@@ -43,7 +43,7 @@ const NewsList = () => {
             setLoading(false);
         };
         fetchData();
-    }, []);
+    }, [category]);
 
     if (loading) {
         return <NewsListBlock>대기 중 ...</NewsListBlock>
